31954aaedb8b96fc40e6d77645807b02f840f176
lala-p <sangmicell@gmail.com>
2022-06-07 13:29:29 +0900

    styled-components 환경 구축하기

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Btn1 = styled.button`
+  width: 10px;
+  height: 100px;
+`
+
 
 const Home = () => {
   return (
     <div>
       it's home.
+      <Btn1>xcvxcvcxvcv</Btn1>
+
     </div>
   )
 }
